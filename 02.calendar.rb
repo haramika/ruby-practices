8b00b10e5390b3d01d66a666f47b3366f854e8c0
haramika <genemika0515@icloud.com>
2025-08-11 20:02:27 +0900

    端的な表現に修正

--- 02.calendar.rb
+++ 02.calendar.rb
@@ -13,22 +13,21 @@ opt.parse!(ARGV)
 today = Date.today
 
 target_year = params[:y].to_i
-if params[:y].to_i == 0
+if target_year == 0
   target_year = today.year
 end
 target_month = params[:m].to_i
-if params[:m].to_i == 0
+if target_month == 0
   target_month = today.month
 end
 
 first_date = Date.new(target_year,target_month,1)
 last_date = Date.new(target_year,target_month,-1)
-cal_date = first_date..last_date
 
-if params[:m].to_i == 0
+if target_month == 0
   puts "#{today.month}月 #{today.year}".center(20)
 else
-  puts "#{params[:m].to_i}月 #{params[:y].to_i}".center(20)
+  puts "#{target_month}月 #{target_year}".center(20)
 end
 
 WEEKDAYS = %w[日 月 火 水 木 金 土]
@@ -36,14 +35,9 @@ WEEKDAYS.each do |w|
   print w.rjust(2)
 end
 
-puts "\n"
+puts
 print "   " * first_date.wday
-cal_date.each do |d|
-  if d.saturday?
-    print d.strftime('%e').rjust(3)
-    puts "\n"
-  else
-    print d.strftime('%e').rjust(3)
-  end
+(first_date..last_date).each do |date|
+  print date.day.rjust(3)
+  puts if date.saturday?
 end
-
