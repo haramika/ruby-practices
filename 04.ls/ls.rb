c8696da3dd094f127b713648209b798d8fa891f1
haramika <genemika0515@icloud.com>
2025-08-26 00:51:27 +0900

    lsコマンド課題を提出

---
+++ 04.ls/ls.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+current_directories = Dir.children('../')
+
+def remove_dot(directory_name)
+  directory_name.delete_if do |n|
+    n.start_with?('.')
+  end
+end
+
+def print_arry(directory_name, multiple, remainder)
+  directory_name.map.with_index do |n, idx|
+    print n.ljust(30) if idx % multiple == remainder
+  end
+  puts
+end
+
+remove_dot(current_directories)
+
+print_arry(current_directories, 5, 0)
+print_arry(current_directories, 5, 1)
+print_arry(current_directories, 5, 2)
+print_arry(current_directories, 5, 3)
+print_arry(current_directories, 5, 4)
