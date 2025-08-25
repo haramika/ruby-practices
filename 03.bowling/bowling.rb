a5b65922a508cdefb3005eabbd1f28840bd296c3
haramika <genemika0515@icloud.com>
2025-08-25 19:16:28 +0900

    frame.sumを統一

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -10,14 +10,17 @@ shots = scores.flat_map { |s| s == 'X' ? [10, 0] : s.to_i }
 frames = shots.each_slice(2).to_a
 
 total_point = frames.each_with_index.sum do |frame, i|
-  if frame[0] == 10 && i < 9
-    frame = frame.sum + frames[i + 1].sum
-    frame += frames[i + 2].first if frames[i + 1].first == 10
-    frame
-  elsif frame.sum == 10 && i < 9
-    frame.sum + frames[i + 1].first
-  else
-    frame.sum
-  end
+  frame.sum + if i >= 9
+                0
+              elsif frame[0] == 10
+                frame = frames[i + 1].sum
+                frame += frames[i + 2].first if frames[i + 1].first == 10
+                frame
+              elsif frame.sum == 10
+                frames[i + 1].first
+              else
+                0
+              end
 end
+
 puts total_point
