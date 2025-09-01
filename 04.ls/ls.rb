2731bad0ccb09b049c8e6817ea6edd98dbd29bcd
haramika <genemika0515@icloud.com>
2025-09-01 11:25:54 +0900

    Dir.globへ修正、メソッド名修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,25 +2,15 @@
 
 # frozen_string_literal: true
 
-current_directories = Dir.entries('../')
+current_directories = Dir.glob("*")
 
-def remove_dot(directory_name)
-  directory_name.delete_if do |file|
-    file.start_with?('.')
-  end
-end
-
-def print_arry(directory_name, multiple, remainder)
+def print_directory(directory_name, multiple, remainder)
   directory_name.map.with_index do |file, idx|
-    print file.ljust(30) if idx % multiple == remainder
+    print file.ljust(20) if idx % multiple == remainder
   end
   puts
 end
 
-remove_dot(current_directories)
-
-print_arry(current_directories, 5, 0)
-print_arry(current_directories, 5, 1)
-print_arry(current_directories, 5, 2)
-print_arry(current_directories, 5, 3)
-print_arry(current_directories, 5, 4)
+print_directory(current_directories, 3, 0)
+print_directory(current_directories, 3, 1)
+print_directory(current_directories, 3, 2)
