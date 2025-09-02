870da0a08ff07c25ffb835a87346e82093bfe1b4
haramika <genemika0515@icloud.com>
2025-09-02 16:12:13 +0900

    ファイル抽出にselectを使用

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -6,14 +6,15 @@ current_directories = Dir.glob('*')
 column_count = (current_directories.size / 3.0).ceil
 
 def main(directory_names, column)
-  (0..column).to_a.each do |remainder|
+  (0..(column - 1)).to_a.each do |remainder|
     print_a_directory_line(directory_names, column, remainder)
   end
 end
 
 def print_a_directory_line(directory_names, column, remainder)
-  directory_names.each_with_index do |file, idx|
-    print file.rjust(3).ljust(20) if idx % column == remainder
+  selected_files = directory_names.select.with_index { |_file, idx| idx % column == remainder }
+  selected_files.each do |file|
+    print file.rjust(3).ljust(20)
   end
   puts
 end
