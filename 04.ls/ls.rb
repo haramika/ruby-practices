7ea07100d0a9fd686a10824c037e5240835e9800
haramika <genemika0515@icloud.com>
2025-09-03 22:45:35 +0900

    ファイル出力メソッドを修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -3,20 +3,18 @@
 # frozen_string_literal: true
 
 current_directories = Dir.glob('*')
-number_of_column = current_directories.size.ceildiv(3)
+number_of_row = current_directories.size.ceildiv(3)
 
 def main(directory_names, max_column)
-  (0..(max_column - 1)).to_a.each do |row|
-    print_directory_line(directory_names, max_column, row)
-  end
+  max_column.times { |row| print print_directory_line(directory_names, max_column, row) }
 end
 
 def print_directory_line(directory_names, max_column, row)
-  selected_files = directory_names.select.with_index { |_file, idx| idx % max_column == row }
+  selected_files = directory_names.values_at(row, row + max_column, row + max_column * 2).compact
   selected_files.each do |file|
-    print file.rjust(3).ljust(20)
+    print file.ljust(20).insert(20, ' ')
   end
   puts
 end
 
-main(current_directories, number_of_column)
+main(current_directories, number_of_row)
