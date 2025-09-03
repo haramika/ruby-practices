de6d62526c88dc5dcfba66fd1e67124350d3eaf8
haramika <genemika0515@icloud.com>
2025-09-03 15:14:27 +0900

    変数名を修正②

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -3,20 +3,20 @@
 # frozen_string_literal: true
 
 current_directories = Dir.glob('*')
-column_count = (current_directories.size / 3.0).ceil
+number_of_column = (current_directories.size / 3.0).ceil
 
-def main(directory_names, column)
-  (0..(column - 1)).to_a.each do |remainder|
-    print_a_directory_line(directory_names, column, remainder)
+def main(directory_names, max_column)
+  (0..(max_column - 1)).to_a.each do |row|
+    print_directory_line(directory_names, max_column, row)
   end
 end
 
-def print_a_directory_line(directory_names, column, remainder)
-  selected_files = directory_names.select.with_index { |_file, idx| idx % column == remainder }
+def print_directory_line(directory_names, max_column, row)
+  selected_files = directory_names.select.with_index { |_file, idx| idx % max_column == row }
   selected_files.each do |file|
     print file.rjust(3).ljust(20)
   end
   puts
 end
 
-main(current_directories, column_count)
+main(current_directories, number_of_column)
