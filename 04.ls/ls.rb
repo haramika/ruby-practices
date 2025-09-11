ca65a45145032d45c1db900ab3cc2fb9c2aaea47
haramika <genemika0515@icloud.com>
2025-09-12 01:06:49 +0900

    メソッド定義を追加

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env
+#!/usr/bin/env ruby
 
 # frozen_string_literal: true
 
@@ -10,54 +10,68 @@ current_directories = Dir.glob('*')
 
 number_of_row = current_directories.size.ceildiv(3)
 
-def ls-l(directory_names)
+def put_total_block(directory_names)
+  total_blocks = directory_names.map do |file|
+    File::Stat.new(file).blocks
+  end
+  print 'total '
+  puts total_blocks.sum
+end
+
+def put_file_detail(directory_names)
   directory_names.each do |file|
     fs = File::Stat.new(file)
 
-  file_types = {
-    '-' => 'file',
-    'd' => 'directory',
-    'l' => 'link'
-  }
+    file_types = {
+      '-' => 'file',
+      'd' => 'directory',
+      'l' => 'link'
+    }
 
-  file_types.each do |k, v|
-    print k if fs.ftype == v  
-  end
+    file_types.each do |k, v|
+      print k if fs.ftype == v
+    end
 
-  permissions = {
-    '---' => '0',
-    '--x' => '1',
-    '-w-' => '2',
-    '-wx' => '3',
-    'r--' => '4',
-    'r-x' => '5',
-    'rw-' => '6',
-    'rwx' => '7',
-  }
+    permissions = {
+      '---' => '0',
+      '--x' => '1',
+      '-w-' => '2',
+      '-wx' => '3',
+      'r--' => '4',
+      'r-x' => '5',
+      'rw-' => '6',
+      'rwx' => '7'
+    }
 
-    permissions.each do |k, v|
-      [3..5].each do |i|
+    [3, 4, 5].each do |i|
+      permissions.each do |k, v|
         print k if fs.mode.to_s(8).rjust(6, '0').slice(i) == v
       end
     end
 
-  ls_contents = [
-    fs.nlink.to_s.rjust(2),
-    Etc.getpwuid(fs.uid).name,
-    Etc.getgrgid(fs.gid).name.rjust(6),
-    fs.size.to_s.rjust(5),
-    fs.mtime.month.to_s.rjust(2),
-    fs.mtime.day.to_s.rjust(2),
-    fs.mtime.strftime('%H:%M'),
-    file
-  ]
+    ls_contents = [
+      fs.nlink.to_s.rjust(2),
+      Etc.getpwuid(fs.uid).name,
+      Etc.getgrgid(fs.gid).name.rjust(6),
+      fs.size.to_s.rjust(5),
+      fs.mtime.month.to_s.rjust(2),
+      fs.mtime.day.to_s.rjust(2),
+      fs.mtime.strftime('%H:%M'),
+      file
+    ]
 
-    ls_contents.each do |code|
-      print ' '  
-      print code
-    end 
+    ls_contents.each do |content|
+      print ' '
+      print content
+    end
+
     puts
   end
+end
+
+def ls_l(directory_names)
+  put_total_block(directory_names)
+  put_file_detail(directory_names)
 end
 
 def main(directory_names, max_column)
@@ -70,4 +84,4 @@ def print_directory_line(directory_names, max_column, row)
   puts
 end
 
-ls-l(current_directories)
+options['l'] ? ls_l(current_directories) : main(current_directories, number_of_row)
