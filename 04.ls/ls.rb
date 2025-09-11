0006778a5b5620328465bdf40ed3e13414b49373
haramika <genemika0515@icloud.com>
2025-09-11 18:41:18 +0900

    lsコマンドを作る4課題提出

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,14 +1,64 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env
 
 # frozen_string_literal: true
 
 require 'optparse'
+require 'etc'
 
-options = ARGV.getopts('r')
+options = ARGV.getopts('l')
 current_directories = Dir.glob('*')
-selected_directories = options['r'] ? current_directories.reverse : current_directories
 
 number_of_row = current_directories.size.ceildiv(3)
+
+def ls-l(directory_names)
+  directory_names.each do |file|
+    fs = File::Stat.new(file)
+
+  file_types = {
+    '-' => 'file',
+    'd' => 'directory',
+    'l' => 'link'
+  }
+
+  file_types.each do |k, v|
+    print k if fs.ftype == v  
+  end
+
+  permissions = {
+    '---' => '0',
+    '--x' => '1',
+    '-w-' => '2',
+    '-wx' => '3',
+    'r--' => '4',
+    'r-x' => '5',
+    'rw-' => '6',
+    'rwx' => '7',
+  }
+
+    permissions.each do |k, v|
+      [3..5].each do |i|
+        print k if fs.mode.to_s(8).rjust(6, '0').slice(i) == v
+      end
+    end
+
+  ls_contents = [
+    fs.nlink.to_s.rjust(2),
+    Etc.getpwuid(fs.uid).name,
+    Etc.getgrgid(fs.gid).name.rjust(6),
+    fs.size.to_s.rjust(5),
+    fs.mtime.month.to_s.rjust(2),
+    fs.mtime.day.to_s.rjust(2),
+    fs.mtime.strftime('%H:%M'),
+    file
+  ]
+
+    ls_contents.each do |code|
+      print ' '  
+      print code
+    end 
+    puts
+  end
+end
 
 def main(directory_names, max_column)
   max_column.times { |row| print print_directory_line(directory_names, max_column, row) }
@@ -20,4 +70,4 @@ def print_directory_line(directory_names, max_column, row)
   puts
 end
 
-main(selected_directories, number_of_row)
+ls-l(current_directories)
