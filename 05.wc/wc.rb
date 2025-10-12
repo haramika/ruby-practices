e510ab9d2d4306b41b118a6dc931f9dedbeea81e
haramika <genemika0515@icloud.com>
2025-10-12 16:26:07 +0900

    各項目の計算結果をハッシュへ格納

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -12,43 +12,44 @@ def main(arguments, options)
 end
 
 def select_file(arguments)
-  arguments.empty? ? [readlines] : arguments.map { |file| File.readlines(file) }
+  arguments.empty? ? [readlines.join] : arguments.map { |file| File.read(file) }
 end
 
-def make_content(arguments, options)
-  file_lines = []
-  file_words = []
-  file_bytes = []
-
+def make_content(arguments)
   select_file(arguments).map do |file|
-    file_lines.push(file.length)
-    file_words.push(file.map { |file| file.split.size }.sum)
-    file_bytes.push(file.join.bytesize)
+    { line: file.lines.count,
+      word: file.split.size,
+      byte: file.bytesize }
   end
+end
 
-  option_contents = []
-  option_contents.push(file_lines) if options['l']
-  option_contents.push(file_words) if options['w']
-  option_contents.push(file_bytes) if options['c']
-
-  options['l'] | options['w'] | options['c'] ? option_contents : [file_lines, file_words, file_bytes]
+def select_content(arguments, options)
+  if options['l'] | options['w'] | options['c']
+    make_content(arguments).each do |content|
+      content.delete(:line) unless options['l']
+      content.delete(:word) unless options['w']
+      content.delete(:byte) unless options['c']
+    end
+  else
+    make_content(arguments)
+  end
 end
 
 def put_content(arguments, options)
-  if arguments.empty?
-    make_content(arguments, options).each { |content| content.each { |content| print content.to_s.rjust(8) } }
+  select_content(arguments, options).each.with_index do |content, i|
+    content.each_value { |value| print value.to_s.rjust(8) }
+    print [' ', arguments[i]].join
     puts
-  else
-    arguments.size.times do |i|
-      make_content(arguments, options).each { |content| print content[i].to_s.rjust(8) }
-      print [' ', arguments[i]].join
-      puts
-    end
   end
 end
 
 def put_total(arguments, options)
-  make_content(arguments, options).each { |n| print n.sum.to_s.rjust(8) }
+  merge_data = select_content(arguments, options).inject do |v1, v2|
+    v1.merge(v2) do |_key, oldval, newval|
+      oldval + newval
+    end
+  end
+  merge_data.each_value { |value| print value.to_s.rjust(8) }
   print ' total'
   puts
 end
